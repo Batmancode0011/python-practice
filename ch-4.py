ef96193ce2b40e18783f59fd4bd11529782764f1
Manish <namish9305@gmail.com>
2026-06-14 19:47:05 +0530

    day 4 -Dictionary and set

--- ch-4.py
+++ ch-4.py
@@ -1,8 +1,33 @@
-info ={
+"""info ={
     "name" : "manish",
     "cgpa" : 9.6,
     "marks" : [97,98,95],
     "subjects" : ("physics","java","c")
+
+}"""
+
+"""
+print(type(info))
+print(info)
+print(info["subjects"])"""
+"""info ["name"] = " Manish Kumar"# to change previous value to this comand
+info["surname"] = "Kumar"# for this command add a new key in the dictonary 
+nulldict = {} # to create a new empty dict 
+nulldict["name"] = "anchal"# To add new key in empty dict we use this 
+print(info)"""
+
+# NESTED Dictionaries 
+"""Dictionary ke ander dictionary """
+"""student = {
+    
+    "name" : "manish kuamr",
+    "subjects": {
+        "physivs" : 95,
+        "python" : 98,
+        "maths" : 99,
+    }
 }
 
-print(info)
+print(student)# for print full student dict
+print(student["subjects"])# for print only subjects
+print(student["subjects"]["maths"])# for only print maths marks"""
