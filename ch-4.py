305609a7df1c9bc70ad3a70c73a785c8a5b7bf52
Manish <namish9305@gmail.com>
2026-06-15 16:27:36 +0530

    day4 - dictionary and set

--- ch-4.py
+++ ch-4.py
@@ -5,6 +5,7 @@
     "subjects" : ("physics","java","c")
 
 }"""
+0
 
 """
 print(type(info))
@@ -31,3 +32,71 @@ print(info)"""
 print(student)# for print full student dict
 print(student["subjects"])# for print only subjects
 print(student["subjects"]["maths"])# for only print maths marks"""
+
+
+
+#SET
+"""
+collection = {1,2,3,3,4,5,4,"hello", "world","hello"}
+print(collection)
+print(len(collection))# total number of items
+
+#collection = set{}# empty set syntax
+"""
+
+
+
+#Practice Questions
+
+#Q1 store following word meanings in a python dictionary:
+"""table : a piece of furniture , lsit of facts& figures
+    cat : a small animal"""
+#solution
+
+"""dict ={
+    "table" : ("a piece of furniture","lsit of facts& figures"),
+    "cat" : "a small animal"
+
+}
+print(dict)
+"""
+#Q2 you are given a list of subject for student.
+# assume one classroom is requires for 1 subject .
+# how many classrooms are needed by all students.
+# "python","java","c++","python","javascipt","java","python","java","c++","c".
+
+# solution
+"""
+subjects = ["python", "java", "c++", "python", "javascript", "java", "python", "java", "c++", "c"]
+
+unique_subjects = set(subjects)
+
+print("Unique Subjects:", unique_subjects)
+print("Classrooms needed:", len(unique_subjects))
+"""
+#Q3 WAP to enter marks of 3 subjects gfrom th euseer and store them in a dict.
+# Start with an empty dict & add one by one. 
+# Use subject name as key & marks as value.
+
+
+#solution 
+
+"""
+marks= {}
+
+x= int(input("enter phy : "))
+marks.update({"phy" : x})
+
+y= int(input("enter chem : "))
+marks.update({"chem" : y})
+
+z= int(input("enter math : "))
+marks.update({"math" : z})
+
+print(marks)
+"""
+
+#Q4 Figure out a way to store 9 & 9.0 as seprate values in the set
+
+value = {("int",9), ("float",9.0)}
+print(value)
